1d89eb7530ef6096bb81f545b5bd0f3e2b005a7b
Tejaswini Samanta <tejaswini.2024.1705@mirandahouse.ac.in>
2026-09-12 01:15:53 +0530

    added final html, css and javascript

--- src/index.js
+++ src/index.js
@@ -49,6 +49,13 @@ function handleSearchSubmit(event) {
     searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"];
+
+    return days[date.getDay()];
+}
+
 function getForecast(city) {
     let apiKey = "2c00a5b0tfa996f784d39a0b16ee6fo7";
     let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -59,21 +66,25 @@ function getForecast(city) {
 function displayForecast(response) {
     let forecastElement = document.querySelector("#forecast");
 
-    let days = ["Sun", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"];
+    
     let forecastHtml = "";
 
-    days.forEach(function(day) {
+    response.data.daily.forEach(function(day, index) {
+        if (index < 6) {
         forecastHtml = forecastHtml + 
         `<div class="weather-forecast-day">
-                <div class="weather-forecast-date">${day}</div>
-                <div class="weather-forecast-icon"></div>
+                <div class="weather-forecast-date">${formatDay(day.time)}</div>
+                <div>
+                <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+                </div>
                 <div class="weather-forecast-temperatures">
                     <div class="weather-forecast-temperature">
-                        <strong>15°</strong>
+                        <strong>${Math.round(day.temperature.maximum)}°</strong>
                     </div>
-                    <div class="weather-forecast-temperature">9°</div>
+                    <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}°</div>
                 </div>
         </div>`;
+        }
     });
 
     forecastElement.innerHTML = forecastHtml;
@@ -83,4 +94,12 @@ function displayForecast(response) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity("Paris");
+function updateCity() {
+    let cityTimeZone = moment.tz.guess();
+
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+
+    searchCity(cityName);
+}
+
+updateCity();
