541b940fba6758f204265915a6032f838665df14
Tejaswini Samanta <tejaswini.2024.1705@mirandahouse.ac.in>
2026-09-11 21:57:11 +0530

    added js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = searchInput.value;
+
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+    // make api call and update the interface
+    let apiKey = "2c00a5b0tfa996f784d39a0b16ee6fo7";
+    let upiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${upiKey}`;
+    axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
 
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
