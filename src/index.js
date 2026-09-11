5aecc9d1efe86b28c6150657331bee394d6d1f6a
Tejaswini Samanta <tejaswini.2024.1705@mirandahouse.ac.in>
2026-09-11 23:19:36 +0530

    added js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ function refreshWeather(response) {
     let timeElement = document.querySelector("#time");
     let date = new Date(response.data.time * 1000);
 
+    let iconElement = document.querySelector("#icon");
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+
     cityElement.innerHTML = response.data.city;
     timeElement.innerHTML = formatDate(date);
     descriptionElement.innerHTML = `${response.data.condition.description}`;
@@ -20,16 +23,8 @@ function formatDate(date) {
     let day = date.getDay();
     let minutes = date.getMinutes();
     let hours = date.getHours();
-    let days = [
-        "Sunday", 
-        "Monday", 
-        "Tuesday",
-        "Wednesday", 
-        "Thursday", 
-        "Friday", 
-        "Saturday"
-    ];
-    let day = days[date.getDay()];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    day = days[date.getDay()];
 
     if (minutes < 10) {
         minutes = `0${minutes}`;
