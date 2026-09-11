a121035133a5fda00559b5b37af185bf2cc65e94
Tejaswini Samanta <tejaswini.2024.1705@mirandahouse.ac.in>
2026-09-11 22:28:45 +0530

    injected more data

--- src/index.js
+++ src/index.js
@@ -2,16 +2,47 @@ function refreshWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
 
     cityElement.innerHTML = response.data.city;
-    temperatureElement.innerHTML = Math.round(temperature);
+    timeElement.innerHTML = formatDate(date);
+    descriptionElement.innerHTML = `${response.data.condition.description}`;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    temperatureElement.innerHTML = `${Math.round(temperature)}`;
+}
+
+function formatDate(date) {
+    let day = date.getDay();
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = [
+        "Sunday", 
+        "Monday", 
+        "Tuesday",
+        "Wednesday", 
+        "Thursday", 
+        "Friday", 
+        "Saturday"
+    ];
+    let day = days[date.getDay()];
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    return `${day} ${hours}:${minutes}`;
+
 }
 
 function searchCity(city) {
     // make api call and update the interface
     let apiKey = "2c00a5b0tfa996f784d39a0b16ee6fo7";
-    let upiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${upiKey}`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
     axios.get(apiUrl).then(refreshWeather);
 }
 
